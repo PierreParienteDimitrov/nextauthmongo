736b9f81819fc23b0a1b1750bc4e8112f8d03419
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-06-03 10:45:42 -0700

    added Vercel

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,28 @@
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
+
+const options = {
+	providers: [
+		Providers.GitHub({
+			clientId: '',
+			clientSecret: '',
+		}),
+		Providers.Twitter({
+			clientId: '',
+			clientSecret: '',
+		}),
+		Providers.Email({
+			server: {
+				host: '',
+				port: '',
+				auth: {
+					user: '',
+					pass: '',
+				},
+			},
+			from: '',
+		}),
+	],
+};
+
+export default (req, res) => NextAuth(req, res, options);
