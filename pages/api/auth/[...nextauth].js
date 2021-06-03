a0e4bea33aaed48baa781711a18813e8a5093132
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2021-06-03 15:11:28 -0700

    added email login"

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,18 +11,19 @@ const options = {
 			clientId: process.env.TWITTER_ID,
 			clientSecret: process.env.TWITTER_SECRET,
 		}),
-		// Providers.Email({
-		// 	server: {
-		// 		host: '',
-		// 		port: '',
-		// 		auth: {
-		// 			user: '',
-		// 			pass: '',
-		// 		},
-		// 	},
-		// 	from: '',
-		// }),
+		Providers.Email({
+			server: {
+				host: process.env.EMAIL_SERVER_HOST,
+				port: process.env.EMAIL_SERVER_PORT,
+				auth: {
+					user: process.env.EMAIL_SERVER_USER,
+					pass: process.env.EMAIL_SERVER_PASSWORD,
+				},
+			},
+			from: process.env.EMAIL_FROM,
+		}),
 	],
+	database: process.env.MONGODB_URI,
 };
 
 export default (req, res) => NextAuth(req, res, options);
